860a0be77b83ec89e5ade08c14b76917bd248fdd
nhughes <nigel_hughes@mentor.com>
2014-07-04 16:17:30 +0100

    Added Mac demonstration project

---
+++ Mac/Tokenizer/Tokenizer/Tokenizer/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Tokenizer
+//
+//  Created by Nigel Hughes on 04/07/2014.
+//  Copyright (c) 2014 Swift Studies. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ Mac/Tokenizer/Tokenizer/TokenizerTests/TokenizerTests.swift
@@ -0,0 +1,35 @@
+//
+//  TokenizerTests.swift
+//  TokenizerTests
+//
+//  Created by Nigel Hughes on 04/07/2014.
+//  Copyright (c) 2014 Swift Studies. All rights reserved.
+//
+
+import XCTest
+
+class TokenizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
