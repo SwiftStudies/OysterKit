87b84e7ef4533447d8b19f7e1928de2560f68239
SwiftStudies <redwhenexcited@icloud.com>
2018-01-17 13:58:23 +0000

    Fixed #14 - Add quick help for all public types for scanning and lexing

--- Sources/OysterKit/Lexical Analysis/Lexer.swift
+++ Sources/OysterKit/Lexical Analysis/Lexer.swift
@@ -1,9 +1,27 @@
+//    Copyright (c) 2014, RED When Excited
+//    All rights reserved.
 //
-//  Lexer.swift
-//  OysterKit
+//    Redistribution and use in source and binary forms, with or without
+//    modification, are permitted provided that the following conditions are met:
 //
-//  Copyright © 2016 RED When Excited. All rights reserved.
+//    * Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
 //
+//    * Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+//    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 import Foundation
 
@@ -47,17 +65,31 @@ private class Mark : CustomStringConvertible {
     }
 }
 
+/**
+ `Lexer` provides a concrete implementation of the `LexicalAnalyzer` protocol and is used by default for the rest of the OysterKit stack (for example
+ in `Language.stream<N:Node,L:LexicalAnalyzer>(lexer:L)->AnySequence<N>`).
+ 
+ In addition to the requirements of the `LexicalAnalyzer` protocol, this implementation provides the ability to skip characters from a `CharacterSet` when
+ a call to `mark()` is made. This can be useful when the consumer always wants, for example, to ignore white space in a file.
+ */
 open class Lexer : LexicalAnalyzer, CustomStringConvertible{
     fileprivate let scanner : StringScanner
     private var marks   = [Mark]()
     
+    /// If set, any characters in this set will be skipped when  `mark()` is called
     public var skip: CharacterSet?
     
+    /**
+     Creates a new instance of `Lexer` for the supplied `String`.
+     
+     - Parameter source: The `String` to perform the lexical analysis on
+    */
     public required init(source: String) {
         scanner = StringScanner(source)
         mark()
     }
     
+    /// The current `LexicalContext` of the `Lexer`
     var currentContext : LexicalContext {
         return LexerContext(mark: Mark(uniPosition:scanner.scanLocation), endLocation: scanner.scanLocation, source: scanner.string)
     }
@@ -72,10 +104,14 @@ open class Lexer : LexicalAnalyzer, CustomStringConvertible{
         return mLoc
     }
     
+    /// The integer offset of the current position in characters from the beginning of the `String` being scanned
+    ///
+    ///  - SeeAlso: `index`
     public var position: Int{
         return scanner.string.unicodeScalars.distance(from: scanner.string.unicodeScalars.startIndex, to: scanner.scanLocation)
     }
     
+    /// The index of the `UnicodeScalarView` position of the `String` being scanned
     public var index: String.UnicodeScalarView.Index {
         get {
             return scanner.scanLocation
@@ -86,38 +122,65 @@ open class Lexer : LexicalAnalyzer, CustomStringConvertible{
         }
     }
     
+    /// The current depth of the `Mark` stack used for unwinding failed rules
     public var depth: Int {
         return marks.count
     }
     
+    /// The character at the current scanning position
     public var current : String {
         return "\(scanner.current)"
     }
     
+    /// Mark the position of the scanner. It should be noted that at this point any characters matching `skip` will be skipped. There
+    /// should always be a matching call to either `rewind()` or `proceed()->LexicalContext`
     public func mark() {
         marks.append(Mark(for:self))
     }
     
+    /**
+     Unwind the current scanning state to the previous position. Typically used when a scan for a match has failed and it is
+     necessary to try another match.
+     
+     - SeeAlso: `proceed()->LexicalContext`
+    */
     open func rewind() {
         let mark = marks.removeLast()
         
         scanner.scanLocation = mark.preSkipLocation
     }
     
+    /**
+     Creates a new `LexicalContext` from the start of top mark to the current scanner position. The mark is removed from the stack
+     extending the range of the new top of the mark stack to this position.
+     
+     - Returns: A `LexicalContext` for the current match
+    */
     open func proceed() -> LexicalContext {
         let mark = marks.removeLast()
 
         return LexerContext(mark: mark, endLocation: scanner.scanLocation, source: source)
     }
     
+    /// Removes the top most `Mark` from the stack without creating a new `LexicalContext` effectively advancing the scanning position
+    /// of the new topmost mark to this position.
     open func consume(){
         let _ = marks.removeLast()
     }
     
+    /// `true` if the end of the `String` being scanned has been reached
     public var endOfInput: Bool{
         return scanner.isAtEnd
     }
     
+    /**
+     Scans until the specified terminal is reached in the source `String`. If the end of the `String` is reached before
+     the supplied `String` is matched a `GrammarError` is thrown. If the terminal is matched, the scanning position will
+     be at the end of that match.
+     
+     - Parameter terminal: A string to scan for
+     - SeeAlso: `scanUpTo(terminal:String)`
+    */
     open func scan(terminal: String) throws {
         if !scanner.scan(string: terminal){
             throw GrammarError.matchFailed(token: nil)
@@ -125,38 +188,68 @@ open class Lexer : LexicalAnalyzer, CustomStringConvertible{
         }
     }
     
+    /**
+     Scans until one of the characters in the supplied `CharacterSet` is found. If the end of the source is reached before a
+     character from the set is found a `GrammarError` is thrown. The scanner position will be directly after the matched
+     character.
+     
+     - Parameter oneOf: A `CharacterSet` to scan for
+    */
     open func scan(oneOf: CharacterSet) throws {
         if !scanner.scan(characterFrom: oneOf) {
             throw GrammarError.matchFailed(token: nil)
         }
     }
     
+    /**
+     Scans up to (that is, the position of the scanner will be at the start of the match, not after it) the supplied `String`. If the terminal is not found
+     a `GrammarError` is thrown. The scanner position will be at the first character of the matched `terminal`.
+
+     - Parameter terminal: A string to scan for
+     - SeeAlso: `scan(terminal:String)`
+    */
     open func scanUpTo(terminal:String) throws {
         if !scanner.scanUpTo(string: terminal){
             throw GrammarError.matchFailed(token: nil)
         }
     }
     
+    /**
+     Scans up to one of the characters in the supplied `CharacterSet` is found. If the end of the source is reached before a
+     character from the set is found a `GrammarError` is thrown. The scanner position will be directly at the matched
+     character.
+     
+     - Parameter oneOf: A `CharacterSet` to scan for
+     */
     open func scanUpTo(oneOf terminal:CharacterSet) throws {
         if !scanner.scanUpTo(characterFrom: terminal){
             throw GrammarError.matchFailed(token: nil)
         }
     }
     
+    /**
+     Advances the scanner one character. If the end of the source `String` has already been reached a `GrammarException` will be thrown.
+    */
     open func scanNext() throws {
         if scanner.scanNext() == nil {
             throw GrammarError.matchFailed(token: nil)
         }
     }
  
+    /**
+     Returns the `String` from the `source` representing the supplied range.
+    */
     public subscript(range:Range<String.UnicodeScalarView.Index>)->String {
         return "\(scanner.string.unicodeScalars[range])"
     }
     
+    
+    /// The `String` currently being scanned
     public var source: String{
         return scanner.string
     }
     
+    /// A description of the current state of the `Lexer`
     public var description: String{
         var result = "Scanning: \(scanner.string.count) characters, currently at \(scanner) '\(scanner.current)':\n"
         
--- Sources/OysterKit/Lexical Analysis/Scanner.swift
+++ Sources/OysterKit/Lexical Analysis/Scanner.swift
@@ -1,43 +1,95 @@
+//    Copyright (c) 2014, RED When Excited
+//    All rights reserved.
 //
-//  Scanner.swift
-//  OysterKit
+//    Redistribution and use in source and binary forms, with or without
+//    modification, are permitted provided that the following conditions are met:
 //
-//  Copyright © 2016 RED When Excited. All rights reserved.
+//    * Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
 //
+//    * Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+//    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Foundation
 
+/**
+ A pure Swift implementation of a `String` scanner, that can leverage some of the strengths of swift (over using `NSScanner`). In general consumers of
+ OysterKit do not need to use this class directly as it is abstracted away by a concrete implementation of `Lexical Analyzer`.
+ 
+ At this point (Swift 4.0) the Swift `String` subsystem is still enjoying a reasonable amount of churn so details of the implementation will change to
+ fully leverage any improved performance affordances. However at this point the implementation is based around the `UnicodeScalarView` of a `String`
+ as it enables direct cosumption of other useful classes such as `CharacterSet`.
+ 
+ This may change in the future as Swift strings evolve.
+ */
 public class StringScanner : CustomStringConvertible{
+    
+    /// The string being scanned
     public  let string       : String
+    
+    /// The scalar view of the string
     private let scalars      : String.UnicodeScalarView
+    
+    /// The current scanning index in `scalars`
     public  var scanLocation : String.UnicodeScalarView.Index
 
+    /// `true` if the scanner is at the end of the `String`
     public  var isAtEnd : Bool {
         return scanLocation == scalars.endIndex
     }
     
+    /**
+    Create a new instance of the class.
+ 
+    - Parameter string: The `String` to be scanned
+    */
     public init(_ string:String){
         self.string         = string
         scalars             = string.unicodeScalars
         self.scanLocation   = scalars.startIndex
     }
-    
+ 
+    /// The scalar currently being evaluated (at the "scan head")
     internal var current : UnicodeScalar {
         return string.unicodeScalars[scanLocation]
     }
     
+    /// A useful description of the state of the scanner
     public var description: String{
         return "\(scalars.distance(from: scalars.startIndex, to: scanLocation))"
     }
     
+    /**
+     Moves the scan-head one position forward. It does not do any bounds checking
+    */
     private func advanceLocation(){
         scanLocation = scalars.index(after: scanLocation)
     }
     
+    /**
+     Set the position of the scan-head explicitly
+    */
     private func setLocation(scalarIndex:String.UnicodeScalarView.Index){
         scanLocation = scalarIndex
     }
     
+    /**
+     Advances the scan-head by one position. If it is at the end of the String it will not move (that is the scan-head will not advance past the last character in the `String`.
+     
+     - Returns: The `UnicodeScalar` currently at the new scan-head position, or nil if the end of the `String` has been reached.
+    */
     public func scanNext()->UnicodeScalar?{
         let startLocation = scanLocation
         
@@ -51,17 +103,34 @@ public class StringScanner : CustomStringConvertible{
         return result
     }
 
-    ///: Scans all characters from the supplied set, continuing until the scanner hits a character not in the set. Returns true if at least one is found
+    /**
+     Scans all characters from the supplied set, continuing until the scanner hits a character not in the set.
+     
+     - Parameter charactersFrom: The `CharacterSet` to scan for
+     - Returns: `true` if at least one is found, `false` otherwise
+    */
     public func scan(charactersFrom set:CharacterSet)->Bool{
         return scan(charactersFrom: set, maximumLength: nil)
     }
 
-    ///: Scan a single character from thet set, returning true if the character is found
+    /**
+     Scans a single character from the supplied set
+     
+     - Parameter charactersFrom: The `CharacterSet` to scan for
+     - Returns: `true` if one is found, `false` otherwise
+     */
     public func scan(characterFrom set:CharacterSet)->Bool{
         return scan(charactersFrom: set, maximumLength: 1)
     }
     
-    
+    /**
+     Scans all characters from the supplied set, continuing until the scanner hits a character not in the set or the maximum length is reached.
+     
+     - Parameter charactersFrom: The `CharacterSet` to scan for
+     - Parameter maximumLength: The maximum number of characters to scan for (scanning will successfully stop if the limit is reached). If `nil` then no
+     limit is assumed
+     - Returns: `true` if at least one is found, `false` otherwise
+     */
     private func scan(charactersFrom set:CharacterSet, maximumLength:Int?)->Bool {
         assert((maximumLength ?? 1) > 0, "If set maximumLength must be greater than 0")
         
@@ -93,7 +162,12 @@ public class StringScanner : CustomStringConvertible{
         return false
     }
 
-    ///: Scan a single character from thet set, returning true if the character is found
+    /**
+     Advances the scan-head until a character from the supplied `CharacterSet` is found, or the end of the `String` is reached.
+     
+     - Parameter charactersFrom: The `CharacterSet` that should stop scanning
+     - Returns: `true` if a character from the supplied set is found before the end of the string.
+    */
     public func scanUpTo(characterFrom set:CharacterSet)->Bool{
         let startPosition = scanLocation
         
@@ -109,6 +183,12 @@ public class StringScanner : CustomStringConvertible{
         return true
     }
     
+    /**
+     Advances the scan-head until a character from the supplied `String` is found, or the end of the `String` is reached.
+     
+     - Parameter string: The `String` that should stop scanning
+     - Returns: `true` if `string` is found before the end of the source.
+     */
     public func scanUpTo(string:String)->Bool{
         let startingPosition = scanLocation
         
@@ -145,6 +225,12 @@ public class StringScanner : CustomStringConvertible{
         return false
     }
     
+    /**
+     Advances the scan-head to the end of the supplied string providing it matches.
+     
+     - Parameter string: The `String` that should be matched
+     - Returns: `true` if `string` is sfound strating from the current scan-head position and before the end of the source.
+     */
     public func scan(string:String)->Bool{
         let startingPosition = scanLocation
         
