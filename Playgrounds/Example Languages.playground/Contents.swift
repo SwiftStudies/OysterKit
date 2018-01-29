ff21b83af03047eb675bf32deee9e7565afaaa9c
SwiftStudies <redwhenexcited@icloud.com>
2018-01-29 15:43:48 +0000

    Added a playground
    Can just be dragged into a generated project to experiment with OysterKit and STLR

--- Playgrounds/Example Languages.playground/Contents.swift
+++ Playgrounds/Example Languages.playground/Contents.swift
@@ -1,5 +1,26 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import Foundation
+import STLR
+import OysterKit
 
-var str = "Hello, playground"
+guard let source = try? String(contentsOfFile: "/Volumes/Personal/SPM/OysterKit/Sources/ExampleLanguages/Grammars/XML.stlr") else {
+    print("Could not load grammar")
+    exit(0)
+}
+
+
+let stlrParser = STLRParser.init(source: source)
+
+stlrParser.grammar[0].produces
+
+
+let ast : DefaultHeterogeneousAST = stlrParser.ast.runtimeLanguage!.build(source: "<hello attr='eh>world</hello>")
+
+for error in ast.errors {
+    print(" - \(error)")
+}
+
+ast.errors
+
+//print(stlrParser.ast.swift(grammar: "XMLTest")!)
