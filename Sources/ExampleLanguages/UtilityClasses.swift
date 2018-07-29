e230e5af25543131a000dcc3b41d79d1d917b6cc
SwiftStudies <redwhenexcited@icloud.com>
2018-07-29 11:25:36 +0100

    Added utilitiy function for tests

--- Sources/ExampleLanguages/UtilityClasses.swift
+++ Sources/ExampleLanguages/UtilityClasses.swift
@@ -23,6 +23,7 @@
 //    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Foundation
+import OysterKit
 
 public struct StringCodingKey : CodingKey {
     public var stringValue: String
@@ -47,4 +48,11 @@ public func dumpDecoder(decoder:Decoder) throws {
     for key in keyedContainer.allKeys {
         print("\t-\(key.stringValue)")
     }
+}
+
+func test(_ rule:BehaviouralRule, with source:String) throws ->MatchResult{
+    let lexer = Lexer(source: source)
+    let ir = AbstractSyntaxTreeConstructor(with: source)
+    
+    return try rule.match(with: lexer, for: ir)
 }
