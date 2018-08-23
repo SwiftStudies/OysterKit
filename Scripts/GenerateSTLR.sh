b069bdf3cc2b211865294da696fdc54288d1055d
SwiftStudies <redwhenexcited@icloud.com>
2018-08-23 08:45:49 +0100

    Added a testing step into the resource generation script

--- Scripts/GenerateSTLR.sh
+++ Scripts/GenerateSTLR.sh
@@ -1,1 +1,5 @@
-swift run stlrc generate -g Resources/STLR.stlr -l swift -o -ot Sources/STLR/HandMade/
+#First generates and puts in Example languages for testing
+swift run stlrc generate -g Resources/STLR.stlr -l swift -o -ot Sources/ExampleLanguages/
+
+#If testing is successful copies it into the correct location of GeneratedSources
+swift test && cp Sources/ExampleLanguages/STLR.swift Sources/STLR/GeneratedSources/_STLR.swift
