4691b453d24c06605b886dc925addd8691b673c5
SwiftStudies <redwhenexcited@icloud.com>
2018-09-01 07:36:40 +0100

    Playing with playground

--- Playgrounds/Experiment.playground/Contents.swift
+++ Playgrounds/Experiment.playground/Contents.swift
@@ -1,40 +1,26 @@
 import Foundation
 import OysterKit
+import STLR
 
-STLRTokens.group.rule.shortDescription
-STLRTokens.group.rule.description
-let groupRecursive = STLRTokens.group.rule as! BehaviouralRecursiveRule
-let expressionRecursive = STLRTokens.rule.rule as? ReferenceRule
-groupRecursive.behaviour.describe(match: "")
-groupRecursive.surrogateRule
+let source = """
+grammar Arrows
 
-let groupSource = """
-(.letter)
+@forArrow  arrow  = ">" arrows?
+@forArrows arrows = arrow
 """
 
-let ruleSource = """
-a = \(groupSource)
-"""
-
-let grammarSource = """
-grammar Test
-
-\(ruleSource)
-"""
-
-do {
-    let ast1 = try AbstractSyntaxTreeConstructor(with: groupSource).build(using: Parser(grammar:[STLRTokens.element.rule]))
-    print("\(ast1)")
-    let ast2 = try AbstractSyntaxTreeConstructor(with: ruleSource).build(using: Parser(grammar:[STLRTokens.rule.rule]))
-    print("\(ast2)")
+do{
+    
+    let dynamicLanguage = try Parser(grammar:STLR._STLR.build(source).grammar.dynamicRules)
+    let arrowsRule = (dynamicLanguage.grammar[0] as? BehaviouralRecursiveRule)?.surrogateRule
+    
+    print(String(repeating: "\n", count: 4))
+    
+    let arrowSource = ">"
+    let lexer = Lexer(source: arrowSource)
+    let ir = AbstractSyntaxTreeConstructor(with: arrowSource)
+    
+    try dynamicLanguage.grammar[0].test(with: lexer, for: ir)
 } catch {
     print("\(error)")
-}
-
-do {
-    let rule = "identifier = (.letter)"
-    let result = try _STLR.build("grammar Test\n\n"+rule)
-    print("Done!")
-} catch {
-    print("Unexpected error: \(error)")
 }
